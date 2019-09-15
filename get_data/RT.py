7159f69c735667f946c96edd05473967f6cdad1b
universcom <hamed.enayatzare@gmail.com>
2019-09-15 23:58:28 +0430

    first-commit

--- get_data/RT.py
+++ get_data/RT.py
@@ -1,12 +1,14 @@
-import time , requests
+import time , requests , datetime
 
 start = time.time()
 f = open("RTs.txt" , "w")
 payload = {"id": "1' and if (ascii(substr(database(), 1, 1))=115,sleep(3),null) --+"}
 # r = requests.get('http://127.0.0.1/sqli-labs/Less-9', params=payload)
-requests.get('http://192.168.0.142')
+LB_ip = '192.168.0.142'
+requests.get('http://' + LB_ip)
 # time.sleep(5)
 roundtrip = (time.time() - start) * 1000
-# print ("%0.2f" %(float(roundtrip)))
-
-RT=round(roundtrip, 2)
+RT= round(roundtrip, 3)
+f = open("./RT_logs.txt", "ar")
+f.write("%s,%s\n" % (str(datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%dT%H:%M:%S')) , str(RT)))
+f.close()
--- get_data/data_gathering.py
+++ get_data/data_gathering.py
@@ -13,3 +13,16 @@ def create_NN_file(instance_id , LB_id) :
     instance_incomming_packets_rate_data = round(resp.get_metric_value("network.incoming.packets.rate" , "instance_network_interface" , network_resource_id), 3)
     LB_incoming_bytes_data = round(resp.get_metric_value("network.services.lb.incoming.bytes" , "loadBalancer" , LB_id), 3)
     LB_outcoming_bytes_data = round(resp.get_metric_value(" network.services.lb.outgoing.bytes" , "loadBalancer" , LB_id), 3)
+
+
+
+    f = open("./data_rt_rr_statwosysdsk.txt", "ar")
+    f.write("%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f\n" % (RT, cpu_data, memory_data, LB_incoming_bytes_data, LB_outcoming_bytes_data, instance_incomming_bytes_data,instance_incomming_bytes_rate_data,instance_incomming_packets_data,instance_incomming_packets_rate_data))
+    f.close()
+
+
+def read_suit_RT():
+    RT_file = open("./RT_logs.txt", "r")
+    line = RT_file.readline()
+    RT , Clock = line.split(",")
+    return rt
