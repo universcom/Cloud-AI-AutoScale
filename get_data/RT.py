69b57bd2af218303736459bc9c97cd4da6d81848
universcom <hamed.enayatzare@gmail.com>
2019-11-28 09:54:20 +0330

    first commit

--- get_data/RT.py
+++ get_data/RT.py
@@ -8,7 +8,7 @@ while 1:
     if re.status_code == 200 or re.status_code == 201 or re.status_code == 202:
         roundtrip = (time.time() - start)
         RT = roundtrip
-        f = open("RTs.txt" , "aw")
+        f = open("/root/autoscale-cloud/get_data/RTs.txt" , "aw")
         f.write("%s,%s\n" % (str(datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%dT%H:%M:%S')) , str(RT)))
         f.close()
         break
