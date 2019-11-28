fe8c9861bfc040c00145f1453a531f92a6b4c195
universcom <hamed.enayatzare@gmail.com>
2019-11-28 09:51:54 +0330

    first commit

--- get_data/RT.py
+++ get_data/RT.py
@@ -5,10 +5,10 @@ LB_ip = '192.168.0.142'
 while 1:
     start = time.time()
     re=requests.get('http://' + LB_ip)
-    if re.status_code == 200 or re.status_code == 201 or re.status_code == 202
+    if re.status_code == 200 or re.status_code == 201 or re.status_code == 202:
         roundtrip = (time.time() - start)
-        RT= round(roundtrip, 3)
-        f = open("RTs.txt" , "w")
+        RT = roundtrip
+        f = open("RTs.txt" , "aw")
         f.write("%s,%s\n" % (str(datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%dT%H:%M:%S')) , str(RT)))
         f.close()
         break
--- get_data/data_gathering.py
+++ get_data/data_gathering.py
@@ -1,40 +1,24 @@
 from gnocchi import gnocchi_api
 
-def create_NN_file(instance_id , LB_id) :
+def create_NN_file(instance_id) :
     data = gnocchi_api("admin" , "hamed" , "admin")
     instance_resource_id = instance_id
     network_instance_resource_id = data.get_resource_id("instance_network_interface" , instance_id)
     disk_instance_resource_id = data.get_resource_id("instance_disk" , instance_id)
     TIMESTAMP = None
-
-    # cpu_data = resp.get_metric_value("cpu_util" , "instance" , instance_resource_id)[2]
-    # memory_data = resp.get_metric_value("memory.usage" , "instance" , instance_resource_id)[2]
-    # instance_incomming_bytes_data = resp.get_metric_value("network.incoming.bytes" , "instance_network_interface" , network_resource_id)[2]
-    # instance_incomming_bytes_rate_data = resp.get_metric_value("network.incoming.bytes.rate" , "instance_network_interface" , network_resource_id)[2]
-    # instance_incomming_packets_data = resp.get_metric_value("network.incoming.packets" , "instance_network_interface" , network_resource_id)[2]
-    # instance_incomming_packets_rate_data = resp.get_metric_value("network.incoming.packets.rate" , "instance_network_interface" , network_resource_id)[2]
-    # LB_incoming_bytes_data = resp.get_metric_value("network.services.lb.incoming.bytes" , "loadBalancer" , LB_id)[2]
-    # LB_outcoming_bytes_data = resp.get_metric_value(" network.services.lb.outgoing.bytes" , "loadBalancer" , LB_id)[2]
-
-    cpuUtileValue = data.get_metric_value("cpu_util" , "instance" , instance_resource_id , TIMESTAMP)
-    memoryUSageValue = data.get_metric_value("memory.usage" , "instance" , instance_resource_id , TIMESTAMP)
-    incommingPacketRate = data.get_metric_value("network.incoming.packets.rate" , "instance_network_interface" , network_instance_resource_id , TIMESTAMP)
-    outgoingPacketRate = data.get_metric_value("network.outgoing.packets.rate" , "instance_network_interface" , network_instance_resource_id , TIMESTAMP)
-    diskReadPacketRate = data.get_metric_value("disk.device.read.requests.rate" , "instance_disk" , disk_instance_resource_id , TIMESTAMP)
-    diskWritePacketRate = data.get_metric_value("disk.device.write.requests.rate" , "instance_disk" , disk_instance_resource_id , TIMESTAMP)
-
-
-
-
-
-
-    f = open("./data_rt_rr_statwosysdsk.txt", "ar")
-    f.write("%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f\n" % (RT, cpu_data, memory_data, LB_incoming_bytes_data, LB_outcoming_bytes_data, instance_incomming_bytes_data,instance_incomming_bytes_rate_data,instance_incomming_packets_data,instance_incomming_packets_rate_data))
-    f.close()
-
-
-def read_suit_RT():
-    RT_file = open("./RT_logs.txt", "r")
-    line = RT_file.readline()
-    RT , Clock = line.split(",")
-    return rt
+    with open("/root/autoscale-cloud/get_data/RTs.txt") as RT_file:
+        lines = RT_file.readlines()
+        for line in lines :
+            TIMESTAMP , rt = line.split(",")
+            RT = float(RT) * 1000
+            cpuUtileValue = data.get_metric_value("cpu_util" , "instance" , instance_resource_id , TIMESTAMP)
+            memoryUSageValue = data.get_metric_value("memory.usage" , "instance" , instance_resource_id , TIMESTAMP)
+            incommingPacketRate = data.get_metric_value("network.incoming.packets.rate" , "instance_network_interface" , network_instance_resource_id , TIMESTAMP)
+            outgoingPacketRate = data.get_metric_value("network.outgoing.packets.rate" , "instance_network_interface" , network_instance_resource_id , TIMESTAMP)
+            diskReadPacketRate = data.get_metric_value("disk.device.read.requests.rate" , "instance_disk" , disk_instance_resource_id , TIMESTAMP)
+            diskWritePacketRate = data.get_metric_value("disk.device.write.requests.rate" , "instance_disk" , disk_instance_resource_id , TIMESTAMP)
+            f = open("./data_rt_rr_statwosysdsk.txt", "ar")
+            f.write("%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f\n" % (RT, cpuUtileValue, memoryUSageValue
+            , incommingPacketRate, outgoingPacketRate, diskReadPacketRate,diskWritePacketRate))
+        f.close()
+create_NN_file("3b4419bb-b94f-45a7-b699-3e9c9e3bc108")
