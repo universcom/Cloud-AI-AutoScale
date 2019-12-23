74fa6cccd704c9b4609545b0e82b21458fc9924a
universcom <hamed.enayatzare@gmail.com>
2019-12-23 11:59:54 +0330

    first commit

--- opestack-autoscale/Resource_Trining_File_Create.py
+++ opestack-autoscale/Resource_Trining_File_Create.py
@@ -27,15 +27,17 @@ def main_proccess():
         #time.sleep(90)
         Now_Resource_usage = Resources_Usage(instance_id , TIMESTAMP)
         print Now_Resource_usage
-        if Now_RT > upper_RT :
+        w = osc.workerInit()
+        while Now_RT > upper_RT :
             k += 1
             print "k is %s" %(k)
-            w = osc.workerInit()
             print "w is %s" %(w)
             osc.addWorker()
             time.sleep(60)
             TIMESTAMP = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%S')
             New_Resource_usage = Resources_Usage(instance_id , TIMESTAMP)
+            Now_RT , TIMESTAMP = Response_Time()
+            print "now RT is: %s" %(Now_RT)
             print New_Resource_usage
             Resource_Net_Create_File =  open("/root/main_code/autoscale-cloud/opestack-autoscale/Resorce_Net_data.txt", "aw")
             Resource_Net_Create_File.write("%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%i,%i\n" %
@@ -43,23 +45,27 @@ def main_proccess():
             ,Now_Resource_usage[0], Now_Resource_usage[1], Now_Resource_usage[2], Now_Resource_usage[3], Now_Resource_usage[4],Now_Resource_usage[5]
             ,k ,w))
             Resource_Net_Create_File.close()
-
-        elif Now_RT < lower_RT:
+        w = osc.workerInit()
+        k = 0
+        while Now_RT < lower_RT and w>1:
             k -= 1
             print "k is %s" %(k)
-            w = osc.workerInit()
             print "w is %s" %(w)
-            if w > 1:
-                osc.removeWorker()
-                sleep(60)
-                TIMESTAMP = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%S')
-                New_Resource_usage = Resources_Usage(instance_id , TIMESTAM)
-                print New_Resource_usage
-                Resource_Net_Create_File =  open("/root/main_code/autoscale-cloud/opestack-autoscale/Resorce_Net_data.txt", "w")
-                Resource_Net_Create_File.write("%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%i,%i\n" %
-                (New_Resource_usage[0], New_Resource_usage[1], New_Resource_usage[2], New_Resource_usage[3], New_Resource_usage[4],New_Resource_usage[5]
-                ,Now_Resource_usage[0], Now_Resource_usage[1], Now_Resource_usage[2], Now_Resource_usage[3], Now_Resource_usage[4],Now_Resource_usage[5]
-                ,k ,w))
-                Resource_Net_Create_File.close()
+            osc.removeWorker()
+            sleep(60)
+            TIMESTAMP = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%S')
+            New_Resource_usage = Resources_Usage(instance_id , TIMESTAM)
+            Now_RT , TIMESTAMP = Response_Time()
+            print "now RT is: %s" %(Now_RT)
+            print New_Resource_usage
+            Resource_Net_Create_File =  open("/root/main_code/autoscale-cloud/opestack-autoscale/Resorce_Net_data.txt", "w")
+            Resource_Net_Create_File.write("%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%i,%i\n" %
+            (New_Resource_usage[0], New_Resource_usage[1], New_Resource_usage[2], New_Resource_usage[3], New_Resource_usage[4],New_Resource_usage[5]
+            ,Now_Resource_usage[0], Now_Resource_usage[1], Now_Resource_usage[2], Now_Resource_usage[3], Now_Resource_usage[4],Now_Resource_usage[5]
+            ,k ,w))
+            Resource_Net_Create_File.close()
+            if k + w -1 == 0 :
+                break
+
 
 main_proccess()
