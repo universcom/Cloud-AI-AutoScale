b9903f470d193221023ed65bc325ddd35ee57b4e
universcom <hamed.enayatzare@gmail.com>
2020-01-02 17:29:22 +0330

    first commit

--- opestack-autoscale/RT.py
+++ opestack-autoscale/RT.py
@@ -1,15 +1,30 @@
+# import time , requests , datetime
+#
+# def Response_Time():
+#     RT_array = []
+#     LB_ip = '192.168.0.142'
+#     while 1:
+#         start = time.time()
+#         re=requests.get('http://' + LB_ip)
+#         if re.status_code == 200 or re.status_code == 201 or re.status_code == 202:
+#             roundtrip = (time.time() - start)
+#             Now_RT= float(roundtrip) * 1000
+#             TIMESTAMP = datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%dT%H:%M:%S')
+#             RT_array.append(Now_RT)
+#             RT_array.append(TIMESTAMP)
+#             return RT_array
+
 import time , requests , datetime
 
-def Response_Time():
-    RT_array = []
-    LB_ip = '192.168.0.142'
-    while 1:
-        start = time.time()
-        re=requests.get('http://' + LB_ip)
-        if re.status_code == 200 or re.status_code == 201 or re.status_code == 202:
-            roundtrip = (time.time() - start)
-            Now_RT= float(roundtrip) * 1000
-            TIMESTAMP = datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%dT%H:%M:%S')
-            RT_array.append(Now_RT)
-            RT_array.append(TIMESTAMP)
-            return RT_array
+
+LB_ip = '192.168.0.142'
+while 1:
+    start = time.time()
+    re=requests.get('http://' + LB_ip)
+    if re.status_code == 200 or re.status_code == 201 or re.status_code == 202:
+        roundtrip = (time.time() - start)
+        RT = roundtrip
+        f = open("/root/autoscale-cloud/get_data/RTs.txt" , "aw")
+        f.write("%s,%s\n" % (str(datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%dT%H:%M:%S')) , str(RT)))
+        f.close()
+        break
